36b57c0b492bb4aa2fb542cd6059d9f49009759a
Kateryna Chaika <gabbeano@gmail.com>
2025-08-05 21:37:45 +0200

    pick the icons for tabs

--- components/TabBar.tsx
+++ components/TabBar.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, TouchableOpacity } from 'react-native';
 import { Link } from 'expo-router';
 import { Pressable } from 'react-native';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
 import AntDesign from '@expo/vector-icons/AntDesign';
-import Ionicons from '@expo/vector-icons/Ionicons';
-import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import Entypo from '@expo/vector-icons/Entypo';
 
 
 
@@ -16,13 +14,13 @@ import Colors from '@/constants/Colors';
 const tabIcons = [
   {
     id: 0,
-    icon: () => <Ionicons name="home-outline" size={24} color="rgb(40 40 40)" />,
-    iconFocused: () => <Ionicons name="home" size={24} color="rgb(150 150 150)" />
+    icon: () => <MaterialIcons name="home-filled" size={24} color="black" />,
+    iconFocused: () => <MaterialIcons name="home-filled" size={24} color="rgb(150 150 150)" />
   },
   {
     id: 1,
-    icon: () => <SimpleLineIcons name="graph" size={24} color="rgb(40 40 40)" />,
-    iconFocused: () => <SimpleLineIcons name="graph" size={24} color="rgb(150 150 150)" />
+    icon: () => <Entypo name="bar-graph" size={24} color="rgb(40 40 40)" />,
+    iconFocused: () => <Entypo name="bar-graph" size={24} color="rgb(150 150 150)" />
   },
   {
     id: 3,
@@ -31,8 +29,8 @@ const tabIcons = [
   },
   {
     id: 4,
-    icon: () => <Ionicons name="settings-outline" size={24} color="rgb(40 40 40)" />,
-    iconFocused: () => <Ionicons name="settings" size={24} color="rgb(150 150 150)" />
+    icon: () => <MaterialIcons name="settings" size={24} color="rgb(40 40 40)" />,
+    iconFocused: () => <MaterialIcons name="settings" size={24} color="rgb(150 150 150)" />
   }
 ]
 
@@ -40,7 +38,13 @@ export default function TabBar({ state, descriptors, navigation }) {
     // const { buildHref } = useLinkBuilder();
 
   return (
-    <View style={{ flexDirection: 'row' }}>
+    <View style={
+      {
+        flexDirection: 'row',
+        backgroundColor: '#fff',
+        borderTopWidth: 1,
+        borderTopColor: 'rgb(40 40 40)'
+      }}>
       {state.routes.map((route, index) => {
         const { options } = descriptors[route.key];
         const label =
@@ -83,9 +87,9 @@ export default function TabBar({ state, descriptors, navigation }) {
                       styles.addButton
                     ]}>
                       <AntDesign
-                        name="pluscircleo"
-                        size={60}
-                        color='rgb(40 40 40)'
+                        name="plus"
+                        size={25}
+                        color='#fff'
                         // color={Colors[colorScheme ?? 'light'].text}
                         style={{ opacity: pressed ? 0.5 : 1 }}
                       />
@@ -123,7 +127,7 @@ export default function TabBar({ state, descriptors, navigation }) {
 const styles = StyleSheet.create({
   tab: {
     flex: 1,
-    height: 60,
+    height: 70,
     alignItems: 'center',
     justifyContent: 'center',
     paddingBottom: 20,
@@ -132,7 +136,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 50,
-    marginTop: -30,
-    backgroundColor: 'white'
+    width: 70,
+    height: 70,
+    marginTop: -20,
+    backgroundColor: 'rgb(40 40 40)'
   }
 });
